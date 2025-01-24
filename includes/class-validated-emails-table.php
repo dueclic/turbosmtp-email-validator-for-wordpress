b8c25a38a77e807f28837d8aee4bec36c7e35a2f
Andrea Debernardi <andrea@debbaweb.it>
2025-01-24 13:01:22 +0100

    table styling filters

--- includes/class-validated-emails-table.php
+++ includes/class-validated-emails-table.php
@@ -55,7 +55,8 @@ class Validated_Emails_Table extends WP_List_Table {
 		$status_links = [];
 
 		foreach ($statuses as $status) {
-			$status_links[$status['status']] = sprintf(__('<a href="%s">%s (%d)</a>', 'turbosmtp-email-validator-for-woocommerce'), admin_url( 'options-general.php?page=email-validation-settings&status='.$status['status'] ), ucfirst($status['status']), $status['total']);
+			$is_status_selected = (isset($_REQUEST['status']) && $_REQUEST['status'] === $status['status']);
+			$status_links[$status['status']] = sprintf(__('<a style="'.($is_status_selected ? 'font-weight:bold': '').'" href="%s">%s (%d)</a>', 'turbosmtp-email-validator-for-woocommerce'), admin_url( 'options-general.php?page=email-validation-settings&status='.$status['status'] ), ucfirst($status['status']), $status['total']);
 		}
 
 		return $status_links;
